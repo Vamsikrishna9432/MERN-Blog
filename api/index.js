ffd5f67483fe2c777674fab56efe421f0044cdf7
Vamsikrishna9432 <krishnavamsi9432@gmail.com>
2024-01-25 14:33:15 +0530

    Connected Mongodb database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('Mongodb connected succesfully');
+}).catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 
