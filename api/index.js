c287f00cde954eb973de5a604a83a903364269c1
Vamsikrishna9432 <krishnavamsi9432@gmail.com>
2024-01-25 15:37:19 +0530

    setup user model, routes, controllers and test api

--- api/index.js
+++ api/index.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
+const app = express();
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log('Mongodb connected succesfully');
 }).catch((err) => {
     console.log(err);
-})
+});
 
-const app = express();
+
+app.use('/api/user', userRoutes);
+
 
 app.listen(3000, () => {
     console.log('server running at http://localhost:3000');
-})
+});
