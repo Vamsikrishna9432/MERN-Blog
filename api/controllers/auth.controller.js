527566c94731bbc455339a67ebd81b0a7e46cbdc
Vamsikrishna9432 <krishnavamsi9432@gmail.com>
2024-01-27 18:51:02 +0530

    created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js';
 import bcryptjs from "bcryptjs";
 import {errorHandler} from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+
 
 export const Signup = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -24,3 +26,34 @@ export const Signup = async (req, res, next) => {
          next(error);
     }
 };
+
+
+export const Signin = async(req, res, next) => {
+     const {email, password} = req.body;
+
+     if (!email || !password || email === "" || password === "") {
+       next(errorHandler(400, 'All feilds are required'));
+     }
+
+     try{
+         const validUser = await User.findOne({email});
+
+         if (!validUser) {
+           return next(errorHandler(400, 'User not Founded'))
+         }
+
+         const passwordValid = bcryptjs.compareSync(password, validUser.password);
+         if (!passwordValid) {
+            return next(errorHandler(404, 'Invalid Password'))
+         }
+
+         const token = jwt.sign({id : validUser._id}, process.env.JWT_SECRET_KEY);
+
+         const {password : pass, ...rest} = validUser._doc;
+
+         res.status(200).cookie('access_token',token,{httpOnly : true}).json(rest);
+
+     }catch(err){
+         next(err)
+     }
+}
